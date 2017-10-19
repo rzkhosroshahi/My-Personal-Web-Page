d8bab4bacc128375f6a80d6acec5dae3cdb62fbb
rzkhosroshahi <rzkhosroshahi@gmail.com>
2017-10-19 21:10:32 +0330

    remove some files

--- js/script.js
+++
@@ -1,9 +0,0 @@
-    const body = document.querySelector('body');
-    const slide = document.querySelector('.slide');
-    const checkBox = document.querySelector(".checkbox");
-
-    const night = (e) =>{
-       if(e.target.checked) body.classList.add('night');
-       else if(!e.target.checked) body.classList.remove('night');
-    };
-    window.addEventListener('click',(e) => {if(e.target === checkBox) night(e)}); 
