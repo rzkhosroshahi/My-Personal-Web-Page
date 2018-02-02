3e109c35fb480503ace8de5f52356acfb7c6c594
rzkhosroshahi <rzkhosroshahi@gmail.com>
2018-02-02 17:55:12 +0330

    use local storage for night shift

--- js/script.js
+++ js/script.js
@@ -2,8 +2,29 @@
     const slide = document.querySelector('.slide');
     const checkBox = document.querySelector(".checkbox");
 
-    const night = (e) =>{
-       if(e.target.checked) body.classList.add('night');
-       else if(!e.target.checked) body.classList.remove('night');
+    const checkNightShift = () => {
+        let nightState = JSON.parse(localStorage.getItem('rezaBlueNight'));
+        if(nightState)  {
+            body.classList.add('night');
+            checkBox.checked = nightState;
+        }
+        else {
+            body.classList.remove('night');
+            checkBox.checked = nightState;            
+        }
+    }; 
+
+    checkNightShift(); 
+
+    const nightSlide = (e) =>{
+       if(e.target.checked) { 
+           localStorage.setItem('rezaBlueNight',true);
+           checkNightShift();
+        }
+       else if(!e.target.checked) {
+           localStorage.setItem('rezaBlueNight', false);
+           checkNightShift();
+       }
     };
-    window.addEventListener('click',(e) => {if(e.target === checkBox) night(e)}); 
+
+    window.addEventListener('click',(e) => {if(e.target === checkBox) nightSlide(e)}); 
